f064347e7fa8c9cf145c56e854d263106308e59e
Dominic Chu <dominic.m.chu@gmail.com>
2020-10-28 21:14:06 -0400

    secondary solution without reverse() and split/joins

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,13 +7,27 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-    return str.split('').reverse().join('');
+
+    // solution #1 
     // const arrayOfStr = str.split('');
     // let stringifiedArray = ""; 
     // arrayOfStr.reverse().forEach( (letter) => {
     //     stringifiedArray += letter;
     // })
     // return stringifiedArray
+
+    // optimal #1 
+    // return str.split('').reverse().join('');
+
+    // altnernate without reverse() bc it's a new js built in func
+    let reversedString = "";
+    for (let char of str) {
+        // we're going to go through all of the characters in the string 
+        // and add the previous collection of it behind the new one 
+        reversedString = char + reversedString;
+    }
+    return reversedString;
+
 }
 
 module.exports = reverse;
