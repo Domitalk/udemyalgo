98a6e7fd3bff3aadc31ad289ca7d0f7f8927cc22
Dominic Chu <dominic.m.chu@gmail.com>
2020-11-10 19:58:58 -0500

    simple answer palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -8,17 +8,39 @@
 //   palindrome("abcdefg") === false
 
 function palindrome(str) {
-    let arr = str.split('');
-    let stringifiedReversed = '';
-    arr.forEach((letter) => {
-        stringifiedReversed = letter + stringifiedReversed 
-    });
-    if (str === stringifiedReversed) {
-        return true 
-    } else {
-        return false 
-    };
+    // this was my first glance answer 
+    //
+    // let arr = str.split('');
+    // let stringifiedReversed = '';
+    // arr.forEach((letter) => {
+    //     stringifiedReversed = letter + stringifiedReversed 
+    // });
+    // if (str === stringifiedReversed) {
+    //     return true 
+    // } else {
+    //     return false 
+    // };
+
+    // the simple solution 
+    //
+    // split the string, reverse the string, join the string 
+    // and return whatever comes out from === which will basically come out true or false 
+    const reversed = str.split('').reverse().join('');
+    return str === reversed; 
 }
+
+    // most complicated solution 
+    // reduce takes two args, a variable to add up to, 
+    // and the variable that's going to represent each item on the array 
+    // with modern syntax we don't have to return the value like you would normally in a reduce if it's in one line 
+    // but you still want to return the whole line as a statement 
+    //
+    // in reality reduce takes two args, a function that returns and a starting value 
+    // in this example we're working with strings and we don't want a starting value so an empty string is used at the end of reduce
+    //
+    // return str.split('').reduce((returningIncrementalVariable, iterativeItem) => iterativeItem + returningIncrementalVariable, '');
+
+
 
 // palindrome('abcde ');
 
