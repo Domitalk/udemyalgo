28a649162ab97df58d9ef0c5a152f3e3de059438
Dominic Chu <dominic.m.chu@gmail.com>
2020-11-11 18:08:59 -0500

    simple solution to reversing int with math.sign as aa built in funciton

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -10,25 +10,34 @@
 
 function reverseInt(n) {
 
-
-
+    // shortened solution 
+    // 
+    const reversed = n
+        .toString()
+        .split('')
+        .reverse()
+        .join('');
+    // Math.sign() will return positive 1 or negative 1 
+    // so just mutliplying the final number with it will return the correct pos/neg 
+    // parseInt will ignore anythign that's not a number so the - at the end or the 0 before will be filtered out 
+    return parseInt(reversed) * Math.sign(n)
 
     // first look answer 
-    let reversedString = '';
-
-    if (n > 0) {
-        n.toString().split('').forEach((element) => {
-            reversedString = element + reversedString
-        })
-        return parseInt(reversedString)
-    } else if (n < 0) {
-        (n * -1).toString().split('').forEach((element) => {
-            reversedString = element + reversedString
-        })
-        return (-1 * parseInt(reversedString))
-    } else {
-        return 0;
-    }
+    //
+    // let reversedString = '';
+    // if (n > 0) {
+    //     n.toString().split('').forEach((element) => {
+    //         reversedString = element + reversedString
+    //     })
+    //     return parseInt(reversedString)
+    // } else if (n < 0) {
+    //     (n * -1).toString().split('').forEach((element) => {
+    //         reversedString = element + reversedString
+    //     })
+    //     return (-1 * parseInt(reversedString))
+    // } else {
+    //     return 0;
+    // }
 }
 
 module.exports = reverseInt;
