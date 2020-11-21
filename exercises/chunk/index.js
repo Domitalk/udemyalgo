0ff9a5123defc1b1983e579bab80ea412f5d677c
Dominic Chu <dominic.m.chu@gmail.com>
2020-11-21 13:38:53 -0500

    chunking arrrays

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,28 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    let motherArray = [];
+    let holdingArray = [];
+    let placeholderNumber = 1;
+    for (let i = 0; i < array.length; i++) {
+        if (placeholderNumber < size) {
+            // when we're just adding to holding array
+            holdingArray.push(array[i])
+            placeholderNumber += 1
+        } else {
+            // when we're pushing the holding array 
+            holdingArray.push(array[i])
+            motherArray.push(holdingArray)
+
+            holdingArray = []
+            placeholderNumber = 1
+        } 
+    }
+    if (holdingArray.length > 0) {
+        motherArray.push(holdingArray)
+    }
+    return motherArray;
+}
 
 module.exports = chunk;
--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -14,12 +14,12 @@
 
 function fizzBuzz(n) {
 
-
     // first glance solution
     // using the old school basic iterator and we go on until i < n + 1; 
     // always start with the most constrictivec so that we can weed out first 
     // get to solution by using modulo to check remainder 
     for (let i = 1; i <= n ; i++) {
+        // you can modulo 15 instead just by using basic maths 
         if (i % 3 === 0 && i % 5 === 0) {
             console.log("fizzbuzz")
         } else if (i % 3 === 0) {
