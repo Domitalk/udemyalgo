67ab038e1b287720856c02a983ccbb1c0c2c69ab
Dominic Chu <dominic.m.chu@gmail.com>
2020-11-21 14:03:29 -0500

    chunking properly first and checking length of each chunk to determine if adding new one is nec

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -9,27 +9,48 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 function chunk(array, size) {
-    let motherArray = [];
-    let holdingArray = [];
-    let placeholderNumber = 1;
-    for (let i = 0; i < array.length; i++) {
-        if (placeholderNumber < size) {
-            // when we're just adding to holding array
-            holdingArray.push(array[i])
-            placeholderNumber += 1
-        } else {
-            // when we're pushing the holding array 
-            holdingArray.push(array[i])
-            motherArray.push(holdingArray)
 
-            holdingArray = []
-            placeholderNumber = 1
-        } 
+    // pre-adding array solution 
+    let motherArray = [];
+    let chunkedArrayIndex = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (i === 0) {
+            // if first time 
+            motherArray[0] = [array[i]]
+        } else if (motherArray[chunkedArrayIndex].length < size) {
+            // if there is room to add element into chunk
+            motherArray[chunkedArrayIndex].push(array[i])
+        } else {
+            // if the chunk is full so new one is req 
+            chunkedArrayIndex++
+            motherArray[chunkedArrayIndex] = [array[i]]
+        }
     }
-    if (holdingArray.length > 0) {
-        motherArray.push(holdingArray)
-    }
-    return motherArray;
+    return motherArray
+
+
+    // first glance solution 
+    // let motherArray = [];
+    // let holdingArray = [];
+    // let placeholderNumber = 1;
+    // for (let i = 0; i < array.length; i++) {
+    //     if (placeholderNumber < size) {
+    //         // when we're just adding to holding array
+    //         holdingArray.push(array[i])
+    //         placeholderNumber += 1
+    //     } else {
+    //         // when we're pushing the holding array 
+    //         holdingArray.push(array[i])
+    //         motherArray.push(holdingArray)
+
+    //         holdingArray = []
+    //         placeholderNumber = 1
+    //     } 
+    // }
+    // if (holdingArray.length > 0) {
+    //     motherArray.push(holdingArray)
+    // }
+    // return motherArray;
 }
 
 module.exports = chunk;
