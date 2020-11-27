62db1ed4efd32a23d737709267743c2dbf9e3d49
Dominic Chu <dominic.m.chu@gmail.com>
2020-11-27 17:09:40 -0500

    notes for first glance answer

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,13 +8,15 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
+
 function anagrams(stringA, stringB) {
+    
+}
 
-
-
+function firstGlance(stringA, stringB) {
 
     // first glance answer 
-
+    // just because it's going to happen twice, just make a helper to convert string into a index object
     const makeStringIntoIndex = (string) => {
         let tempObject = {};
         const stringArray = string.replace(/[^\w]/g, "").toLowerCase().split('')
@@ -28,21 +30,26 @@ function anagrams(stringA, stringB) {
         return tempObject;
     }; 
 
+    // make the two index objects
     const indexA = makeStringIntoIndex(stringA)
     const indexB = makeStringIntoIndex(stringB)
 
+    // a breakout clause to see if the length of keys is the same 
     if (Object.keys(indexA).length !== Object.keys(indexB).length) {
         return false; 
     }
 
+    // only one needs to be false so here just make sure that it stays true 
     let holdingAnswer = true 
 
+    // check each index and if any one of them have different values, make holding answer = false 
     Object.keys(indexA).forEach((element) => {
         if (indexA[element] !== indexB[element]) {
             holdingAnswer = false;
         };
     });
 
+    // just return the holding answer 
     return holdingAnswer;
 }
 
