581e629bd8bc9135ed564b39c214a5d9dd958414
Dominic Chu <dominic.m.chu@gmail.com>
2020-11-30 19:33:26 -0500

    streamlined solution to anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -10,7 +10,36 @@
 
 
 function anagrams(stringA, stringB) {
-    
+    const stringACharacterMap = makeStringIntoIndex(stringA)
+    const stringBCharacterMap = makeStringIntoIndex(stringB)
+
+    // first exit case, wherein the keys dont match up so there's no way for it to be correct 
+    // this also makes it so we only have to iterate through one character map and not both 
+    if (Object.keys(stringACharacterMap).length !== Object.keys(stringBCharacterMap).length) {
+        return false;
+    }
+
+    // then, iterate through each, and figure out if all the values match 
+    // object iteration is in 
+    // array iteration is of 
+    // just by looking at this, it's clear that the let char is the key of the object and obviously that's the best thing to iterate with so that the values can be pinpointed
+    for (let char in stringACharacterMap) {
+        if (stringACharacterMap[char] !== stringBCharacterMap[char]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+const makeStringIntoIndex = (string) => {
+    const characterMap = {};
+    // instead of using old school for loop, use the for (of) and don't bother assignming the array to a variable just put it inside 
+    for (let char of string.replace(/[^\w]/g, "").toLowerCase().split('')) {
+        // then this assigns + 1 but if it doesn't exist, it'll make it 1 if the value is invalid 
+        characterMap[char] = characterMap[char] + 1 || 1
+    }
+    return characterMap
 }
 
 function firstGlance(stringA, stringB) {
