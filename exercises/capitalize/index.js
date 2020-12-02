83fda020a5112959255128be43d3d05a557da72f
Dominic Chu <dominic.m.chu@gmail.com>
2020-12-02 12:24:48 -0500

    first glance solution to capitalizing each word in a sentence

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,19 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+
+    // so the idea is to isolate each word first 
+    const stringAsArray = str.split(' ')
+    // console.log(stringAsArray)
+    // then going through a reducer make a string again but each time it iterates, it'll capitalize the first character and add the rest of the word and a space after 
+    return stringAsArray.reduce((accumulator, element) => {
+        // console.log(accumulator)
+        // console.log(element)
+        return accumulator += (element.charAt(0).toUpperCase() + element.slice(1) + ' ')
+        // at the end of the reducer, it'll take out the last space 
+        // another way to do this would be to check the index of the array, which the reducer can make available 
+    }, '').slice(0, -1)
+}
 
 module.exports = capitalize;
