4fd872b2a7e9d62ab88df9158d9af2477ea1e2aa
Dominic Chu <dominic.m.chu@gmail.com>
2020-12-02 17:04:24 -0500

    capitalize solution with edge case to weed out  puncuation

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -45,9 +45,39 @@ function lessonSolutionOne(str) {
     return words.join(' ')
 }
 
-function firstGlance(str) {
+function capitalize(str) {
     // so the idea is to isolate each word first 
     const stringAsArray = str.split(' ')
+
+    // ** stretch goal validate: validating helper to check for special characters 
+    const isAlphabet = (letter) => {
+        // using ES6 to check for alpha doesn't work to filter out special characters 
+        // return letter.toLowerCase() != letter.toUpperCase();
+        return letter.match(/[a-z]/)
+    }; 
+
+    // ** stretch goal validate: helper to iterate through a word and find the first alphabet to capitalize 
+    const specialCharacterEdgeCase = (string) => {
+        let holderString = '';
+        let keepGoing = true; 
+        for (i = 0; i < string.length; i++) {
+            if (!keepGoing) {
+                // just stop 
+                break; 
+            } else if (isAlphabet(string[i]) && keepGoing) {
+                // console.log("keep going")
+                // if it's a string, uppercase and add the rest of the string and flag stop 
+                holderString += string[i].toUpperCase() + string.slice(i + 1) 
+                keepGoing = false;
+            } else {
+                // if it's not a string just add it and go to next one 
+                // console.log("dollar sign trigger")
+                holderString += string[i]
+            }
+        };
+        return holderString;
+    }
+
     // console.log(stringAsArray)
     // then going through a reducer make a string again but each time it iterates, it'll capitalize the first character and add the rest of the word and a space after 
     return stringAsArray.reduce((accumulator, element) => {
@@ -55,10 +85,18 @@ function firstGlance(str) {
         // console.log(element)
         // remember that reducer needs a return value 
         // another way to do this would be just to use index of string [0] instead of using the slice charAt function which is a sort of slice 
-        return accumulator += (element.charAt(0).toUpperCase() + element.slice(1) + ' ')
+
+        // ** stretch goal validate: check if alphabet and if not do a special 
+        if (isAlphabet(element[0])) {
+            return accumulator += (element.charAt(0).toUpperCase() + element.slice(1) + ' ')
+        } else {
+            return accumulator += (specialCharacterEdgeCase(element) + ' ')
+        }
         // at the end of the reducer, it'll take out the last space 
         // another way to do this would be to check the index of the array, which the reducer can make available 
     }, '').slice(0, -1)
 }
+
+
 
 module.exports = capitalize;
